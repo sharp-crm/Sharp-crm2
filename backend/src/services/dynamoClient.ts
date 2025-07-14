682a18ea69ab3ee0a665b41a0f150d6084205d15
Tejaswin <tejaswin@Jaideep-iMac.local>
2025-07-14 18:18:00 +0530

    Resolving Deployment Issues 6

--- backend/src/services/dynamoClient.ts
+++ backend/src/services/dynamoClient.ts
@@ -24,8 +24,8 @@ const getClientConfig = () => {
     return {
       region: "us-east-1",
       // endpoint: "http://localhost:8000",
-      endpoint: "http://127.0.0.1:8000",
-      // endpoint: "http://host.docker.internal:8000",
+      // endpoint: "http://127.0.0.1:8000",
+      endpoint: "http://host.docker.internal:8000",
       credentials: {
         accessKeyId: "fakeMyKeyId",
         secretAccessKey: "fakeSecretAccessKey"
